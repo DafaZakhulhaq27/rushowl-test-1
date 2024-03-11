72dd2941ff9b813b73b7bae8930cf759c0c3c746
Dafa Zakhulhaq <dafa27890@gmail.com>
2024-03-11 16:22:50 +0700

    feat: add login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SyntheticEvent, useCallback, useRef, useState } from "react";
+import "./App.css";
+import fetcher from "./utils/fetcher";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentPage, setCurrentPage] = useState<"login" | "home">("login");
+  const [loading, setIsLoading] = useState<boolean>(false);
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const handleSubmit = useCallback(
+    async (e: SyntheticEvent) => {
+      e.preventDefault();
+      setIsLoading(true);
+      try {
+        await fetcher.post("/auth/login", {
+          // using split("@")[0] because the dummy API only provides login
+          // with a username, while in the requirements test you have to use email.
+          // so I used split to remove the email
+          username: emailRef.current?.value.split("@")[0],
+          password: passwordRef.current?.value,
+        });
+
+        setCurrentPage("home");
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          alert(error.response?.data.message);
+        } else {
+          alert(JSON.stringify(error));
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [emailRef, passwordRef]
+  );
+
+  if (currentPage === "home") {
+    return <h1>Welcome Home !!!</h1>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <form onSubmit={handleSubmit} className="form-container">
+      <h2>Login Form</h2>
+      <div className="form-input">
+        <label htmlFor="email">Username</label>
+        <input
+          ref={emailRef}
+          type="email"
+          name="email"
+          placeholder="Email..."
+          required
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="form-input">
+        <label htmlFor="email">Password</label>
+        <input
+          ref={passwordRef}
+          type="password"
+          placeholder="Password..."
+          required
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <button type="submit" disabled={loading}>
+        {loading ? "Loading" : "Submit"}
+      </button>
+    </form>
+  );
 }
 
-export default App
+export default App;
